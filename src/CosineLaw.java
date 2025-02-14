0b43d9eae4f9dfe985346b44f0f5b675d3a85447
csmandyw <mandy.weng@student.tdsb.on.ca>
2025-02-13 23:57:15 -0500

    completed cosine law

---
+++ src/CosineLaw.java
@@ -0,0 +1,19 @@
+// Cosine Law (Feb.12.2025)
+import java.util.Scanner;
+
+public class CosineLaw {
+    public static void main(String[] args) throws Exception {
+        Scanner keyboard = new Scanner (System.in);
+        
+        System.out.println("Please enter side a");
+        double a = keyboard.nextDouble();
+        System.out.println("Please enter side b");
+        double b = keyboard.nextDouble();
+        System.out.println("Please enter angle C");
+        double angleC = keyboard.nextDouble();
+        keyboard.close();
+
+        double c = Math.sqrt((a*a + b*b) - (2*a*b)*(Math.cos(Math.toRadians(angleC))));
+        System.out.println("The missing side is:" + c);
+    }
+}
