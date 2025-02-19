b2c6d5f0c23dcdb82de0f6f98ac11df559e781ea
csmandyw <mandy.weng@student.tdsb.on.ca>
2025-02-18 20:41:58 -0500

    completed nand gate

--- src/NANDGate.java
+++ src/NANDGate.java
@@ -1,18 +1,18 @@
 // NAND Gate (Feb.18.2025)
-
 import java.util.Scanner;
+
 public class NANDGate {
     public static void main(String[] args) throws Exception {
         Scanner keyboard = new Scanner(System.in);
-        boolean cValue = false;     
-        
-        System.out.println("Please enter your  a value[true/false]");
-        a= keyboard.next().charAt(0);
-        System.out.println("Please enter your b value[/]");
-        b = keyboard.next().charAt(0);
+        boolean a, b, c = false;
+
+        System.out.println("Please enter your a value [true/false]");
+        a = keyboard.nextBoolean();
+        System.out.println("Please enter your b value [true/false]");
+        b = keyboard.nextBoolean();
         keyboard.close();
 
-        boolean cValue = !(a && b == 1)
-
+        c = !(a && b);
+        System.out.println("The NAND value of c is:" + c);
     }
 }
