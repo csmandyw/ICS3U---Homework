619539df0cad2fd3aea203a7152af20ddc0f4522
csmandyw <mandy.weng@student.tdsb.on.ca>
2025-03-15 13:46:23 -0400

    updated code

--- src/ArayTest.java
+++ src/ArayTest.java
@@ -1,26 +1,43 @@
 // Array Test (Mar.4.2025)
-
 import java.util.Scanner;
 
-public class ArayTest {
-    public static void main(String[] args) throws Exception {
-        Scanner keyboard = new Scanner (System.in);
-       
-        System.out.println("Please enter a number between 1 and 4");
-        String[] menu = new String[4];
-        int number = keyboard.nextInt();
-        keyboard.close();
-        
-        if (number == 1) {
-            menu[1] = "Please enter 10 strings into the array";
-        } 
-        menu[2] = "Below are the inputs of the array";
-        menu[3] = "Choose a number between 1 and 10";
-        menu[4] = "";
-        
-       
+public class ArrayTest {
+  public static void main(String[] args) throws Exception {
+    final int STRING_NUMBER = 10;
+    Scanner keyboard = new Scanner(System.in);
+    String[] data = new String[STRING_NUMBER];
 
-          
+    loop: while (true) {
+      System.out.println("Please select an option:");
+      System.out.println("1. Enter " + STRING_NUMBER + " Strings");
+      System.out.println("2. Display all Strings");
+      System.out.println("3. Select specific String");
+      System.out.println("4. Exit");
 
+      int choice = keyboard.nextInt();
+      keyboard.nextLine();
+
+      switch (choice) {
+        case 1:
+          for (int i = 0; i < STRING_NUMBER; i++) {
+            System.out.println("Please enter String number " + (i + 1));
+            data[i] = keyboard.nextLine();
+          }
+          break;
+        case 2:
+          for (int i = 0; i < STRING_NUMBER; i++) {
+            System.out.println(data[i]);
+          }
+          break;
+        case 3:
+          System.out.println("Which String do you want?");
+          choice = keyboard.nextInt();
+          System.out.println(data[choice - 1]);
+          break;
+        case 4:
+          keyboard.close();
+          break loop;
+      }
     }
+  }
 }
