5f700eba3357f5cb59a92ad068d2aed5f7e78a28
unknown <342861705@TDSB.ON.CA>
2025-03-17 11:39:15 -0400

    l

---
+++ src/HelperMethods.java
@@ -0,0 +1,13 @@
+// Helper Methods (Mar.17.2025)
+import java.util.Scanner;
+
+public class HelperMethods {
+    public static double piApproximation(int n) {
+        
+    }
+    
+    
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
